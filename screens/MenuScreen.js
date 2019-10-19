17a42bf5545d81ceb023da1d8d90c3859008c783
leocrossman <lkcrossman@gmail.com>
2019-10-19 15:49:13 -0400

    Edited menu (bolded sections)

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -22,18 +22,26 @@ export default function MenuScreen() {
     >
       <Text style={styles.title}> Den Menu</Text>
       <Text style={styles.text}>
-        Sandwiches: {'\n'}Turkey/Ham/Tuna{'\n'}Chunky Chicken Salad{'\n'}
+        <Text style={styles.bold}>Sandwiches:{'\n'}</Text>
+        Turkey/Ham/Tuna{'\n'}Chunky Chicken Salad{'\n'}
         Egg Salad{'\n'}BLT{'\n'}Hummus & Veggie Wrap{'\n'}Grilled Chicken
         Sandwich with lettuce & tomato on a toasted bun{'\n'}
-        {'\n'}Bread Options: White, Wheat, Twelve Grain, Rye, Borealis, English
-        Muffin, Bagel, Gluten Friendly Bread{'\n'}
-        {'\n'}Additions: Pesto, Humus, Avocado, Extra Cheese{'\n'}
-        {'\n'}From the Grill:{'\n'}Bates Burger{'\n'}Bigcat Burger{'\n'}
+        {'\n'}
+        <Text style={styles.bold}>Bread Options:{'\n'}</Text>
+        White, Wheat, Twelve Grain, Rye, Borealis, English Muffin, Bagel, Gluten
+        Friendly Bread{'\n'}
+        {'\n'}
+        <Text style={styles.bold}>Additions:{'\n'}</Text>
+        Pesto, Humus, Avocado, Extra Cheese{'\n'}
+        {'\n'}
+        <Text style={styles.bold}>From the Grill:{'\n'}</Text>Bates Burger
+        {'\n'}Bigcat Burger{'\n'}
         Hamburger{'\n'}
         Cheeseburger{'\n'}Double Cheeseburger{'\n'}Veggie Burger{'\n'}Grilled
         Cheese{'\n'}Tuna Melt{'\n'}
         {'\n'}
-        Additions: Tomato, Bacon{'\n'}
+        <Text style={styles.bold}>Additions:{'\n'}</Text>
+        Tomato, Bacon{'\n'}
         {'\n'}
         French Fries{'\n'}Onion Rings{'\n'}Chicken Nuggets{'\n'}Wings of Fire
         {'\n'}Mozzarella Sticks (Mozz Sticks){'\n'}Jalapeno Poppers{'\n'}
@@ -55,6 +63,9 @@ const styles = StyleSheet.create({
     fontSize: 32,
     textAlign: 'center',
     paddingVertical: 20,
+  },
+  bold: {
+    fontWeight: 'bold',
   },
   text: {
     color: 'white',
